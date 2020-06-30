095ea564bd22c59c86105f4ae8db974215405fdf
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-06-29 23:59:48 -0500

    updates to FC syntax

--- api/Category/index.ts
+++ api/Category/index.ts
@@ -2,7 +2,9 @@ import fetch from "node-fetch";
 import { Post, EntityId } from "../../shared";
 import { config } from "../Config";
 
-export const fetchPostsByCategory = async (categoryId: EntityId): Promise<Post[]> => {
-    const res = await fetch(`${config.baseUrl}/categories/${categoryId}`);
-    return res.json();
+export const fetchPostsByCategory = async (
+	categoryId: EntityId
+): Promise<Post[]> => {
+	const res = await fetch(`${config.baseUrl}/categories/${categoryId}`);
+	return res.json();
 };
