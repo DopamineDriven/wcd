76872d40900b0f1ce6436059bba1b8c3609ea81f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-04 18:08:41 -0500

    troubleshooting cont

--- components/index.ts
+++ components/index.ts
@@ -2,6 +2,6 @@ export * from "./Feed";
 export * from "./Footer";
 export * from "./Header";
 export * from "./Loader";
-export * from "./PostBody"
+export * from "./PostBody";
 export * from "./Section";
 export * from "./Style";
